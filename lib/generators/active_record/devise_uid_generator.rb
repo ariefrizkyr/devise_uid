25037c0e51cf0074515614e0bfcba09fb09392a7
Arief R Ramadhan <ariefrizkyr@gmail.com>
2023-03-18 12:24:13 +0700

    attempt another fix

--- lib/generators/active_record/devise_uid_generator.rb
+++ lib/generators/active_record/devise_uid_generator.rb
@@ -8,6 +8,20 @@ module ActiveRecord
       def copy_devise_migration
         migration_template "migration.rb", "db/migrate/devise_uid_add_to_#{table_name}.rb", migration_version: migration_version
       end
+
+      def rails5_and_up?
+        Rails::VERSION::MAJOR >= 5
+      end
+
+      def rails61_and_up?
+        Rails::VERSION::MAJOR > 6 || (Rails::VERSION::MAJOR == 6 && Rails::VERSION::MINOR >= 1)
+      end
+
+      def migration_version
+        if rails5_and_up?
+          "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+        end
+      end
     end
   end
 end

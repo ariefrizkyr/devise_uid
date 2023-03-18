a900dcea0f753c6d5fd5bcb736bb058804df5e4a
Arief R Ramadhan <ariefrizkyr@gmail.com>
2023-03-18 12:16:01 +0700

    Updated variable name to version only

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,4 +1,4 @@
-class DeviseUidAddTo<%= table_name.camelize %> < ActiveRecord::Migration[<%= migration_version %>]
+class DeviseUidAddTo<%= table_name.camelize %> < ActiveRecord::Migration[<%= version %>]
   def change
     add_column :<%= table_name %>, :uid, :string
     add_index :<%= table_name %>, :uid, unique: true
